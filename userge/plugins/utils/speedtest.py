9eecbd55f623286fde68053b0173a85d86bd54bb
xbeet <84825812+xbeet@users.noreply.github.com>
2021-06-07 13:08:01 +0530

    Update speedtest.py

--- userge/plugins/utils/speedtest.py
+++ userge/plugins/utils/speedtest.py
@@ -17,13 +17,13 @@ CHANNEL = userge.getCLogger(__name__)
 
 @userge.on_cmd("speedtest", about={'header': "test your server speed"})
 async def speedtst(message: Message):
-    await message.edit("`Running speed test . . .`")
+    await message.edit("`🚀Running speed test . . . . .`")
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        await message.try_to_edit("`Performing download test . . .`")
+        await message.try_to_edit("`🔻Performing download test . . . . .`")
         test.download()
-        await message.try_to_edit("`Performing upload test . . .`")
+        await message.try_to_edit("`🔺Performing upload test . . . . .`")
         test.upload()
         test.results.share()
         result = test.results.dict()
@@ -33,23 +33,23 @@ async def speedtst(message: Message):
     path = await pool.run_in_thread(wget.download)(result['share'])
     output = f"""**--Started at {result['timestamp']}--
 
-Client:
+〓══〓 CLiENT 〓══〓
 
 ISP: `{result['client']['isp']}`
 Country: `{result['client']['country']}`
 
-Server:
+〓══〓 SERVER 〓══〓
 
-Name: `{result['server']['name']}`
-Country: `{result['server']['country']}, {result['server']['cc']}`
-Sponsor: `{result['server']['sponsor']}`
-Latency: `{result['server']['latency']}`
+╭─•🧰 Name: `{result['server']['name']}`
+├─•🕹 Country: `{result['server']['country']}, {result['server']['cc']}`
+├─•💶 Sponsor: `{result['server']['sponsor']}`
+╰─•📡 Latency: `{result['server']['latency']}`
 
-Ping: `{result['ping']}`
-Sent: `{humanbytes(result['bytes_sent'])}`
-Received: `{humanbytes(result['bytes_received'])}`
-Download: `{humanbytes(result['download'] / 8)}/s`
-Upload: `{humanbytes(result['upload'] / 8)}/s`**"""
+╭─•🖲 Ping: `{result['ping']}`
+├─•📤 Sent: `{humanbytes(result['bytes_sent'])}`
+├─•📥 Received: `{humanbytes(result['bytes_received'])}`
+├─•🔻 Download: `{humanbytes(result['download'] / 8)}/s`
+╰─•🔺 Upload: `{humanbytes(result['upload'] / 8)}/s`**"""
     msg = await message.client.send_photo(chat_id=message.chat.id,
                                           photo=path,
                                           caption=output)
