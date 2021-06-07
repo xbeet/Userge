47010122d5b26bec901386617f0b0491d1675d52
xbeet <84825812+xbeet@users.noreply.github.com>
2021-06-07 12:19:46 +0530

    Update repo.py

--- userge/plugins/tools/repo.py
+++ userge/plugins/tools/repo.py
@@ -13,13 +13,13 @@ from userge import userge, Message, Config, versions, get_version
 async def see_repo(message: Message):
     """see repo"""
     output = f"""
-**Hey**, __I am using__ 🔥 **Userge** 🔥
+**Hey**, __I am using__ ⚡ **Userge** ⚡
 
     __Durable as a Serge__
 
-• **userge version** : `{get_version()}`
-• **license** : {versions.__license__}
-• **copyright** : {versions.__copyright__}
-• **repo** : [Userge]({Config.UPSTREAM_REPO})
+╭─• **Userge version** : `{get_version()}`
+├─• **License** : {versions.__license__}
+├─• **Copyright** : {versions.__copyright__}
+╰─• **Repo** : [Userge]({Config.UPSTREAM_REPO})
 """
     await message.edit(output)
