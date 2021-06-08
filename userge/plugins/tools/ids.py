2a2c2573778ab667b6ab6b7648c8b9accbad0745
xbeet <84825812+xbeet@users.noreply.github.com>
2021-06-08 15:15:27 +0530

    Update ids.py

--- userge/plugins/tools/ids.py
+++ userge/plugins/tools/ids.py
@@ -14,10 +14,10 @@ from userge import userge, Message
     'usage': "reply {tr}ids any message, file or just send this command"})
 async def getids(message: Message):
     msg = message.reply_to_message or message
-    out_str = f"👥 **Chat ID** : `{(msg.forward_from_chat or msg.chat).id}`\n"
-    out_str += f"💬 **Message ID** : `{msg.forward_from_message_id or msg.message_id}`\n"
+    out_str = f"╭─•🌈 **Chat ID** : `{(msg.forward_from_chat or msg.chat).id}`\n"
+    out_str += f"├─•💭 **Message ID** : `{msg.forward_from_message_id or msg.message_id}`\n"
     if msg.from_user:
-        out_str += f"🙋‍♂️ **From User ID** : `{msg.from_user.id}`\n"
+        out_str += f"╰─•🕵️‍♂️ **From User ID** : `{msg.from_user.id}`\n"
     file_id = None
     if msg.audio:
         type_ = "audio"
@@ -44,6 +44,6 @@ async def getids(message: Message):
         type_ = "video"
         file_id = msg.video.file_id
     if file_id is not None:
-        out_str += f"📄 **Media Type:** `{type_}`\n"
-        out_str += f"📄 **File ID:** `{file_id}`"
+        out_str += f"╭─•🗂 **Media Type:** `{type_}`\n"
+        out_str += f"╰─•📄 **File ID:** `{file_id}`"
     await message.edit(out_str)
