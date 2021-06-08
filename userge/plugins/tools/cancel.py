2eb4c967acd823137c86fe374247edf1ba67658f
xbeet <84825812+xbeet@users.noreply.github.com>
2021-06-08 15:13:10 +0530

    Update cancel.py

--- userge/plugins/tools/cancel.py
+++ userge/plugins/tools/cancel.py
@@ -15,7 +15,7 @@ async def cancel_(message: Message):
     if replied:
         replied.cancel_the_process()
         await message.edit(
-            "`added your request to the cancel list`", del_in=5)
+            "`added your request to the cancel list🤙`", del_in=5)
     else:
         await message.edit(
-            "`reply to the message you want to cancel`", del_in=5)
+            "`reply to the message you want to cancel🥴`", del_in=5)
