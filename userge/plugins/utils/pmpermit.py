c197e7aafbec78c4a1e39e2eb24c5406553b6c74
xbeet <84825812+xbeet@users.noreply.github.com>
2021-06-09 12:25:09 +0530

    Update pmpermit.py

--- userge/plugins/utils/pmpermit.py
+++ userge/plugins/utils/pmpermit.py
@@ -388,7 +388,7 @@ if userge.has_bot:
         else:
             user_dict = await userge.get_user_dict(c_q.from_user.id)
             await c_q.edit_message_text(
-                noPmMessage.format_map(SafeDict(**user_dict)) + '\n`- Protected by userge`')
+                noPmMessage.format_map(SafeDict(**user_dict)) + '\n\n`- Protected by userge`')
             buttons = InlineKeyboardMarkup(
                 [
                     [
